9b9109f8736ffdf9bad317fd2e54b77e5f49428b
Christopher <chris.tryber@gmail.com>
2021-09-01 13:27:32 -0300

    fix lint

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import Header from "./page/Header";
-import Main from "./page/Main";
-import Footer from "./page/Footer";
-import postAPI from "./services/newsletterAPI";
+import React from 'react';
+import Header from './page/Header';
+import Main from './page/Main';
+import Footer from './page/Footer';
+import postAPI from './services/newsletterAPI';
 
 function App() {
   const teste = {
-    name: "Chris",
-    email: "email@valido.com",
+    name: 'Chris',
+    email: 'email@valido.com',
   };
   postAPI(teste);
 
--- src/services/newsletterAPI.js
+++ src/services/newsletterAPI.js
@@ -1,10 +1,10 @@
 const postAPI = async (reqBody) => {
-  const endPoint = "https://api.jungledevs.com/api/v1/challenge-newsletter/";
+  const endPoint = 'https://api.jungledevs.com/api/v1/challenge-newsletter/';
 
   const options = {
-    mode: "cors",
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
+    mode: 'cors',
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(reqBody),
   };
   try {
