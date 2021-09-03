854e4a3452364f192a17ec05281ce3aff2ef7905
Christopher <chris.tryber@gmail.com>
2021-09-02 23:28:26 -0300

    css fourthSection

--- src/components/SectionFour.jsx
+++ src/components/SectionFour.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { ImageSection3 } from '../images';
+import styles from '../styles/main.module.css';
 
 export default function SectionFour() {
   return (
-    <section>
+    <section className={ styles.fourthSection }>
       <img src={ ImageSection3 } alt="ImageSection3" />
-      <h2>Shared payments made simple</h2>
+      <h3>Shared payments made simple</h3>
       <p>
         {/* eslint-disable-next-line max-len */}
         Sometimes it’s hard enough just sharing a restaurant bill. Try sharing that bill week in, week out and you might encounter more than a few snares. But not with Hapu. Simply set your rates and our automated payment system takes care of the rest. You need never talk money or who owes what.
