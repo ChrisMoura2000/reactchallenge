a3044b2544b0873b8abe0a0af1a7fcec0bd1a321
Christopher <chris.tryber@gmail.com>
2021-09-03 18:07:37 -0300

    Começando css tela maior

--- src/components/FooterUp.jsx
+++ src/components/FooterUp.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Union } from '../images';
+import styles from '../styles/footer.module.css';
 
 export default function FooterUp() {
   return (
-    <div>
+    <div className={ styles.footerUp }>
       <h2>Become a nanny share host</h2>
       <p>Takes less than 5 minutes to get started</p>
       <button type="button">
         <img src={ Union } alt="Union" />
-        <p>Create Your Nanny Share</p>
-        <p>Takes less than 5 minutes</p>
+        <div>
+          <p className={ styles.btnCreateNanny }>Create Your Nanny Share</p>
+          <p>Takes less than 5 minutes</p>
+        </div>
       </button>
       <a href="#browseLocal" id="browseLocal">Or browse local nanny-shares</a>
     </div>
